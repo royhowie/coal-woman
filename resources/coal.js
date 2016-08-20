f30a6c51449d7d7bfe05505bc2221afbb14383f7
Roy <roydhowie@gmail.com>
2016-08-20 13:42:00 -0400

    reworked slide content

--- resources/coal.js
+++ resources/coal.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
+    var coinDropped = false
     $('#fullpage').fullpage({
-        anchors: ['coal', 'coin', 'info', 'donate'],
+        anchors: [1, 2, 3, 4, 5, 6].map(String),
+        // anchors: ['coal', 'coin', 'info', 'donate'],
         lockAnchors: false,
         loopHorizontal: false,
-        navigation: true,
-        navigationPosition: 'right',
+        // navigation: true,
+        // navigationPosition: 'right',
         slidesNavPosition: 'bottom',
         slidesNavigation: true,
+        onLeave: function (index, next, direction) {
+            console.log('args:', arguments)
+
+            if (index == 2 && next == 3 && direction == 'down' && !coinDropped) {
+                console.log('hello!')
+                coinDropped = true
+            }
+        }
     })
 
     // Map each picture to its corresponding slide.
